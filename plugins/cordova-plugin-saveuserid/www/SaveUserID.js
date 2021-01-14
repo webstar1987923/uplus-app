ea14e0127eb8e9d86a211d544630a6989be793d5
webstar <webstar1987923@hotmail.com>
2021-01-14 21:11:44 +0800

    updated

--- plugins/cordova-plugin-saveuserid/www/SaveUserID.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-module.exports.saveUID = function(arg0, success, error) {
-    exec(success, error, "SaveUserID", "saveUID", [arg0]);
-}
-
-module.exports.removeUID = function(arg0, success, error) {
-    exec(success, error, "SaveUserID", "removeUID", [arg0]);
-}
